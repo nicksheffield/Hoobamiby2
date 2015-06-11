e6291dbc1f122393d950dad3a0b014519af62b5e
Nick Sheffield <numbereft@gmail.com>
2015-06-12 00:27:00 +1200

    Redesigned Player api

--- modules/player.js
+++ modules/player.js
@@ -1,13 +1,21 @@
-module.exports = function(io, socket){
-	this.nickname = '';
-	this.room = '';
-	this.blackCards = [];
-	this.whiteCards = [];
-	this.isHost = false;
-	this.isJudge = false;
-	this.waiting = false;
-	this.socketID = '';
-	this.submissions = [];
+var _ = require('lodash');
+
+module.exports = function(socket_id){	
+	this.data = {
+		nickname: '',
+		room: '',
+		blackCards: [],
+		whiteCards: [],
+		isHost: false,
+		isJudge: false,
+		waiting: false,
+		socketID: socket_id,
+		submissions: []
+	};
+
+	this.fill = function(data){
+		this.data = _.merge(this.data, data);
+	};
 
 	this.emit = function(event, data){
 		socket.emit(event, data);
