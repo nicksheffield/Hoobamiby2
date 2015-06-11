0bb20864083030e9dc989368dcdc33863a3e63f1
Nick Sheffield <numbereft@gmail.com>
2015-06-12 00:25:34 +1200

    Streamlined character chooser

--- modules/game.js
+++ modules/game.js
@@ -31,12 +31,7 @@ module.exports = function(io, socket) {
 
 	this.generateRoomName = function(len) {
 		// any letters in here MUST be lowercase. Uppercase will be IMPOSSIBLE to match
-		var chars = [
-			//'1', '2', '3', '4', '5', '6', '7', '8', '9',
-			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k',
-			'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-			'v', 'w', 'x', 'y', 'z'
-		];
+		var chars = 'abcdefghijklmnopqrstuvxyz'.split('');
 		var roomName;
 
 		// generate a room name and check if it's already in use.
